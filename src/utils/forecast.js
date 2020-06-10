894982a2fccbbda4ff6fbede5db04d7e173c3b57
himanshu <himanshumittal496@gmail.com>
2020-06-10 15:15:50 +0530

    Add humidity to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
                 callback('Unable to find location!', undefined)
             }
              else {
-                callback(undefined, body.current.weather_descriptions[0] + '. It is ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.')
+                callback(undefined, body.current.weather_descriptions[0] + '. It is ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out. Humidity here is ' + body.current.humidity + '%.')
              }
     })
 }
